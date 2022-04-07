30533e7b055f464d677b64c39b73556195f51304
ktej11 <cenateja3@gmail.com>
2022-04-07 09:26:02 +0530

    first commit

---
+++ action.js
@@ -0,0 +1,10 @@
+var cat = Object.create(Object.prototype,{
+    name:{
+        value:"fluffy",enumerable:true,writable:true,configurable:true
+    },
+    color:{
+        value:"ehite",enumerable:true,writable:true,configurable:true
+    }
+});
+console.log(cat.name);
+var a= document.getElementById
